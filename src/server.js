bb24604635dbbced8ea1a40f828fe8030a92a574
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-25 09:39:27 +1100

    Add body parser

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const server = express()
+
+// Allow POST/PUT/PATCH requests to pass along JSON, and it will be decoded
+// for us and it will be decoded for us at 'req.json'
+server.use(bodyParser.json())
+
+server.use('/',[
+  require('./routes/artists'),
+  require('./routes/todos')
+])
 
 server.listen(7000, () => {
   console.log('Started at http://localhost:7000')
